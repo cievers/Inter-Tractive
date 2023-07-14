a76f4b6dc5557ce237b2c3ba5cd5376947628155
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-07-14 11:25:32 +0200

    Fixed issue with duplicate metadata entries causing errors, and generally cleaned up tck file reading

---
+++ Assets/Scripts/Files/Meta.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Files {
+	public class Meta {
+		private Dictionary<string, List<string>> values;
+
+		public Meta() {
+			values = new Dictionary<string, List<string>>();
+		}
+		public Meta(Dictionary<string, string> values) {
+			this.values = values.ToDictionary(item => item.Key, item => new List<string> {item.Value});
+		}
+		public Meta(Dictionary<string, List<string>> values) {
+			this.values = values;
+		}
+		
+		public void Add(string key, string value) {
+			if (!values.ContainsKey(key)) {
+				values[key] = new List<string>();
+			}
+			values[key].Add(value);
+		}
+
+		public string Get(string key) {
+			return values[key][^1];
+		}
+		public List<string> GetAll(string key) {
+			return values[key];
+		}
+		public string this[string key] => Get(key);
+	}
+}
--- Assets/Scripts/Files/Types/Tck.cs
+++ Assets/Scripts/Files/Types/Tck.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using Geometry;
@@ -8,31 +9,21 @@ using UnityEngine;
 
 namespace Files.Types {
 	public record Tck : Tractogram {
-		public string Path;
-		public Dictionary<string, string> Metadata;
-		// public Tract[] Lines;
-		public Bounds Bounds;
+		public string path;
+		public Meta metadata;
+		public Bounds boundaries;
 		
 		public IEnumerable<Tract> Tracts {get;}
-		// public Vector3[] Points {
-		// 	get {
-		// 		var result = new List<Vector3>();
-		// 		foreach (var tract in Tracts) {
-		// 			result.AddRange(tract.Points);
-		// 		}
-		// 		return result.ToArray();
-		// 	}
-		// }
 		public IEnumerable<Vector3> Points => Tracts.SelectMany(tract => tract.Points);
 		public Boundaries Boundaries => Boundaries.Join(Tracts.Select(segment => segment.Boundaries));
 		public float Resolution => Tracts.Select(segment => segment.Resolution).Min();
 		public float Slack => Tracts.Select(segment => segment.Slack).Max();
 
-		public Tck(Dictionary<string, string> metadata, Tract[] lines, Bounds bounds, string path) {
-			Metadata = metadata;
+		public Tck(string path, Meta metadata, IEnumerable<Tract> lines, Bounds boundaries) {
+			this.path = path;
+			this.metadata = metadata;
 			Tracts = lines;
-			Bounds = bounds;
-			Path = path;
+			this.boundaries = boundaries;
 		}
 		
 		private const int VOXEL_SIZE = 10;
@@ -40,69 +31,63 @@ namespace Files.Types {
 		private static readonly byte[] READ_BUFFER = new byte[VECTOR_SIZE * 10000];
 
 		public static Tck Load(string path) {
-			if (!BitConverter.IsLittleEndian) throw new Exception("No support yet for big endian architectures");
-
-			List<Vector3> points = new();
-			List<Tract> lines = new();
-
-			var meta = new Dictionary<string, string>();
-
+			if (!BitConverter.IsLittleEndian) {
+				throw new Exception("No support yet for big endian architectures");
+			}
+			
+			// Setup the file reading
 			using var filestream = File.OpenRead(path);
 			using var reader = new StreamReader(filestream);
 
-			//first line has to be 'mrtrix tracks'
-			if ((reader.ReadLine() ?? throw new Exception("Incorrect metadata format")).Trim() != "mrtrix tracks") throw new Exception("File is not in MRtrix format.");
-
-			var metadataLine = reader.ReadLine() ?? throw new Exception("Incorrect metadata format");
-
-			while (metadataLine != "END") {
-				Debug.Log("Reading metedata: "+metadataLine);
-				// var splitted = metadataLine.Split(':', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-				var splitted = metadataLine.Split(':', StringSplitOptions.RemoveEmptyEntries); // TODO: Possibly trim whitespace from resulting splits
-				meta.Add(splitted[0].Trim(), splitted[1].Trim());
-				metadataLine = reader.ReadLine() ?? throw new Exception("Incorrect metadata format");
+			// Start by parsing off the metadata
+			var meta = LoadMeta(reader);
+			if (meta["datatype"] != "Float32LE") {
+				throw new DataException("Only supports Float32LE for now");
 			}
-
-			if (meta["datatype"] != "Float32LE") throw new Exception("Only supports Float32LE for now");
-
-			var binaryInfoOffset = int.Parse(meta["file"].Replace(".", ""));
-			filestream.Position = binaryInfoOffset;
-
+			
+			// Prepare constants and container variables for reading the vertex data
+			const float maxDistance = VOXEL_SIZE * .98f; // TODO: This looks like a hack, and what does VOXEL_SIZE have to do with loading tracts in the first place?
+			const float maxDistanceSqrt = maxDistance * maxDistance;
+			
+			var points = new List<Vector3>();
+			var lines = new List<Tract>();
 			var boundsMin = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
 			var boundsMax = new Vector3(float.MinValue, float.MinValue, float.MinValue);
+			
+			// Set the file streamer position to the start of the vertex data
+			filestream.Position = int.Parse(meta["file"].Replace(".", ""));
 
-			var maxDistance = VOXEL_SIZE * .98f;
-			var maxDistanceSqrt = maxDistance * maxDistance;
-
-			//read all points except last point.
-			long filePointCount = (filestream.Length - filestream.Position) / VECTOR_SIZE - 1;
+			// Read all points except last point.
+			var filePointCount = (filestream.Length - filestream.Position) / VECTOR_SIZE - 1;
 			// lines.EnsureCapacity((int)filePointCount); // TODO: If we get some capacity/out of bounds errors, look here
 
 
-			for (int v = 0; v < filePointCount - 1;) {
-				int readLength = filestream.Read(READ_BUFFER, 0, READ_BUFFER.Length);
+			for (var v = 0; v < filePointCount - 1;) {
+				var readLength = filestream.Read(READ_BUFFER, 0, READ_BUFFER.Length);
 
-				for (int i = 0; i < readLength / VECTOR_SIZE; i++) {
-					if (v >= filePointCount - 1) break;
+				for (var i = 0; i < readLength / VECTOR_SIZE; i++) {
+					if (v >= filePointCount - 1) {
+						break;
+					}
 
 					Vector3 point = GetVectorInBuffer(i);
 
 					if (float.IsNaN(point.x) && float.IsNaN(point.y) && float.IsNaN(point.z)) {
 						Vector3[] pointsArray = points.ToArray();
-						Vector3 averageDirection = Vector3.Normalize(points[points.Count - 1] - points[0]);
+						Vector3 averageDirection = Vector3.Normalize(points[^1] - points[0]);
 						lines.Add(new Tract(pointsArray, (uint)lines.Count, averageDirection, (uint)v));
 						points.Clear();
 					} else {
 						UpdateBounds(ref boundsMin, ref boundsMax, point);
 
-						if (points.Count >= 2 && (points[points.Count - 1] - point).sqrMagnitude > maxDistanceSqrt) {
-							var dis = Vector3.Distance(points[points.Count - 1], point);
-							var interpolationPointCount = MathF.Ceiling(dis / maxDistance);
-							for (int j = 0; j < interpolationPointCount; j++) {
+						if (points.Count >= 2 && (points[^1] - point).sqrMagnitude > maxDistanceSqrt) {
+							var distance = Vector3.Distance(points[^1], point);
+							var interpolationPointCount = (int) MathF.Ceiling(distance / maxDistance);
+							for (var j = 0; j < interpolationPointCount; j++) {
 								//start and endpoints are real points not interpolated.
-								if (j == interpolationPointCount || j == 0) continue;
-
-								points.Add(Vector3.Lerp(points[points.Count - 1], point, j / interpolationPointCount));
+								if (j != interpolationPointCount && j != 0) {
+									points.Add(Vector3.Lerp(points[^1], point, j / interpolationPointCount));
+								}
 							}
 						}
 						points.Add(point);
@@ -110,19 +95,45 @@ namespace Files.Types {
 					v++;
 				}
 			}
-			// //check last point is the correct seperator.
+			// Check last point is the correct separator.
 			// if (GetVectorInBuffer(v) != new Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity))
 			//     throw new Exception("Something went wrong during reading. Final point is not (inf,inf,inf).");
 
-			if (filestream.Position != filestream.Length) throw new Exception("Something went wrong during reading. End of file has not been reached.");
+			if (filestream.Position != filestream.Length) {
+				throw new FileLoadException("Something went wrong during reading. End of file has not been reached.");
+			}
 
-			return new Tck(meta, lines.ToArray(), new Bounds(boundsMin, boundsMax), path);
+			return new Tck(path, meta, lines.ToArray(), new Bounds(boundsMin, boundsMax));
+		}
+		private static Meta LoadMeta(TextReader reader) {
+			var meta = new Meta();
+			
+			// First line has to be 'mrtrix tracks'
+			if (LoadMetaLine(reader).Trim() != "mrtrix tracks") {
+				throw new FileLoadException("File is not in MRtrix format.");
+			}
+
+			var metadataLine = LoadMetaLine(reader);
+
+			while (metadataLine != "END") {
+				Debug.Log("Reading metadata: "+metadataLine);
+				
+				var split = metadataLine.Split(':', StringSplitOptions.RemoveEmptyEntries);
+				meta.Add(split[0].Trim(), split[1].Trim());
+				
+				metadataLine = LoadMetaLine(reader);
+			}
+
+			return meta;
+		}
+		private static string LoadMetaLine(TextReader reader) {
+			return reader.ReadLine() ?? throw new FileLoadException("Incorrect metadata format");
 		}
 
 		private static Vector3 GetVectorInBuffer(int i) {
-			float x = BitConverter.ToSingle(READ_BUFFER, i * VECTOR_SIZE);
-			float y = BitConverter.ToSingle(READ_BUFFER, i * VECTOR_SIZE + sizeof(float));
-			float z = BitConverter.ToSingle(READ_BUFFER, i * VECTOR_SIZE + sizeof(float) * 2);
+			var x = BitConverter.ToSingle(READ_BUFFER, i * VECTOR_SIZE);
+			var y = BitConverter.ToSingle(READ_BUFFER, i * VECTOR_SIZE + sizeof(float));
+			var z = BitConverter.ToSingle(READ_BUFFER, i * VECTOR_SIZE + sizeof(float) * 2);
 			var point = new Vector3(x, z, y);
 			return point;
 		}

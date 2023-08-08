761a5e600981434d5bb641261b587a7278e06b28
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-08-08 09:04:28 +0200

    Added a compound metric as simple way to build multi-dimensional measurements

---
+++ Assets/Scripts/Evaluation/Geometric/CompoundMetric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Geometry.Tracts;
+
+namespace Evaluation.Geometric {
+	public class CompoundMetric : TractMetric {
+		private readonly TractMetric[] metrics;
+		public override int Dimensions => metrics.Sum(metric => metric.Dimensions);
+		public override string[] Units => metrics.SelectMany(metric => metric.Units).ToArray();
+
+		public CompoundMetric(TractMetric[] metrics) {
+			this.metrics = metrics;
+		}
+		
+		public override Vector Measure(IEnumerable<Tract> tracts) {
+			var measured = metrics.Select(metric => metric.Measure(tracts));
+			return measured.Aggregate(new Vector(Array.Empty<float>()), (a, b) => a.Add(b));
+		}
+	}
+}

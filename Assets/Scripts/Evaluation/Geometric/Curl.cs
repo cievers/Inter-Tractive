86cc8e835effa8e4137074ddc01a6fdd14084699
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-08-10 09:18:48 +0200

    New tract metrics for span and curl

---
+++ Assets/Scripts/Evaluation/Geometric/Curl.cs
@@ -0,0 +1,15 @@
+﻿using Geometry.Tracts;
+
+namespace Evaluation.Geometric {
+	public class Curl : CachedMetric {
+		private readonly Length length = new();
+		private readonly Span span = new();
+		
+		public override int Dimensions => 1;
+		public override string[] Units => new[] {"mm"};
+		
+		public override float Measure(Tract tract) {
+			return length.Measure(tract) / span.Measure(tract);
+		}
+	}
+}
---
+++ Assets/Scripts/Evaluation/Geometric/Span.cs
@@ -0,0 +1,13 @@
+﻿using Geometry.Tracts;
+
+namespace Evaluation.Geometric {
+	public class Span : CachedMetric {
+		public override int Dimensions => 1;
+		public override string[] Units => new[] {"mm"};
+		
+		public override float Measure(Tract tract) {
+			var points = tract.Points;
+			return (points[0] - points[^1]).magnitude;
+		}
+	}
+}

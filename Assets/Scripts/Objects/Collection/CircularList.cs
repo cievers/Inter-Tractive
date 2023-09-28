6d1bf63f6730354510d41fe206982f74b295c8e5
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-09-28 10:53:26 +0200

    Basic borrowed implementations of convex hulls in two and three dimensions

---
+++ Assets/Scripts/Objects/Collection/CircularList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Objects.Collection {
+	public class CircularList<T> : List<T> {
+		public T Last {
+			get => this[Count - 1];
+			set => this[Count - 1] = value;
+		}
+
+		public T First {
+			get => this[0];
+			set => this[0] = value;
+		}
+
+		public void PushLast(T item) {
+			Add(item);
+		}
+
+		public T PopLast() {
+			T result = this[Count - 1];
+			RemoveAt(Count - 1);
+			return result;
+		}
+
+		public void PushFirst(T item) {
+			Insert(0, item);
+		}
+
+		public T PopFirst() {
+			T result = this[0];
+			RemoveAt(0);
+			return result;
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/Vector.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Utility {
+	public static class Vector {
+		// TODO: These are only used by ConvexPolygon, but shouldn't that just use these functions from Unity itself?
+		public static Vector2 Sub(this Vector2 a, Vector2 b) {
+			return a - b;
+		}
+
+		public static float Cross(this Vector2 a, Vector2 b) {
+			return a.x * b.y - a.y * b.x;
+		}
+	}
+}

3984049659cbd102161c460b8af126398be66034
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-10-04 11:09:58 +0200

    Cleaned up how cross section is computed

--- Assets/Scripts/Objects/Sources/Progressive/CrossSection.cs
+++ Assets/Scripts/Objects/Sources/Progressive/CrossSection.cs
@@ -39,44 +39,15 @@ namespace Objects.Sources.Progressive {
 		}
 		
 		protected override void Compute() {
-			// Complete(
-			// 	tract.Normals
-			// 		.Select((normal, i) => new ConvexPolygon(
-			// 			Plane.Projections(tractogram.Slice(i), tract.Points[i], normal).ToList(), 
-			// 			tract.Points[i], 
-			// 			normal
-			// 		))
-			// 		.ToList()
-			// 	);
-			// return;
-			var result = new List<ConvexPolygon>();
-			var points = tract.Points;
-			var normals = tract.Normals.ToArray();
-			for (var i = 0; i < tract.Points.Length; i++) {
-				// var origin = tract.Points[1];
-				// var normal = tract.Points[2] - tract.Points[0];
-				// var points = new List<Vector3>();
-				// var tracts = tractogram.Tracts.ToArray();
-				// for (var i = 0; i < 100 ; i++) {
-				// 	points.AddRange(Geometry.Plane.Intersections(tracts[i], origin, normal));
-				// }
-				// foreach (var point in points) {
-				// 	Instantiate(dot, point, Quaternion.identity);
-				// }
-				var projections = Plane.Projections(tractogram.Slice(i), points[i], normals[i]).ToList();
-				var hull = new ConvexPolyhedron(tractogram.Slice(i).ToList());
-				var intersections = Plane.Intersections(hull.Edges, points[i], normals[i]).ToList();
-				// foreach (var projection in projections) {
-				// 	Instantiate(dot, projection, Quaternion.identity);
-				// }
-				var perimeter = new ConvexPolygon(intersections, points[i], normals[i]);
-				// foreach (var point in perimeter.Points) {
-				// 	Instantiate(dot, point, Quaternion.identity);
-				// }
-				// hull.Add(perimeter.Hull());
-				result.Add(perimeter);
-			}
-			Complete(result);
+			Complete(
+				tract.Normals
+					.Select((normal, i) => new ConvexPolygon(
+						Plane.Intersections(new ConvexPolyhedron(tractogram.Slice(i).ToList()).Edges, tract.Points[i], normal).ToList(), 
+						tract.Points[i], 
+						normal
+					))
+					.ToList()
+			);
 		}
 	}
 }

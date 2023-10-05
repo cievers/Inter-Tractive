9c38c5f08679eb19fe2f10fb220b97b88c485017
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-10-05 10:44:39 +0200

    Cleanup end face filtering

--- Assets/Scripts/Geometry/Generators/ConvexWrap.cs
+++ Assets/Scripts/Geometry/Generators/ConvexWrap.cs
@@ -20,13 +20,10 @@ namespace Geometry.Generators {
 			var solid = new ConvexPolyhedron(start.Points.Concat(end.Points).ToList()).Hull();
 			var result = new List<int>();
 			for (var i = 0; i < solid.Indices.Length; i += 3) {
-				var point0 = solid.Vertices[solid.Indices[i]];
-				var point1 = solid.Vertices[solid.Indices[i+1]];
-				var point2 = solid.Vertices[solid.Indices[i+2]];
-				var point0InStart = points.Contains(point0);
-				var point1InStart = points.Contains(point1);
-				var point2InStart = points.Contains(point2);
-				if (!((point0InStart && point1InStart && point2InStart) || (!point0InStart && !point1InStart && !point2InStart))) {
+				var contains0 = points.Contains(solid.Vertices[solid.Indices[i]]);
+				var contains1 = points.Contains(solid.Vertices[solid.Indices[i+1]]);
+				var contains2 = points.Contains(solid.Vertices[solid.Indices[i+2]]);
+				if (contains0 ^ contains1 || contains1 ^ contains2) {
 					result.Add(i);
 					result.Add(i+1);
 					result.Add(i+2);

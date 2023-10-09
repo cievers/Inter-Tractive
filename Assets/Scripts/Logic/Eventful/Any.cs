1073aa3f4ccb29923ef41a0dec84d2df851edd93
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-10-09 16:35:11 +0200

    Created an aggregate boolean state representing the OR operator

---
+++ Assets/Scripts/Logic/Eventful/Any.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Logic.Eventful {
+	public class Any : Boolean {
+		private readonly IEnumerable<Boolean> inputs;
+		public bool State {get; private set;}
+
+		public Any(IEnumerable<Boolean> inputs) {
+			var array = inputs as Boolean[] ?? inputs.ToArray();
+			this.inputs = array;
+			foreach (var state in array) {
+				state.Change += _ => Update();
+			}
+		}
+
+		private void Update() {
+			var current = inputs.Any(input => input.State);
+			if (State != current) {
+				Update(current);
+			}
+		}
+		private void Update(bool state) {
+			State = state;
+			Change?.Invoke(State);
+			if (State) {
+				True?.Invoke();
+			} else {
+				False?.Invoke();
+			}
+		}
+		
+		public event Boolean.ValueEvent True;
+		public event Boolean.ValueEvent False;
+		public event Boolean.ChangeEvent Change;
+	}
+}

2a9b25bc5cd9a17650bf472b6ab09a88e696c3b0
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-10-14 11:30:15 +0200

    Added a simple definition of a tractogram, which the uniform tractogram now extends

--- Assets/Scripts/Geometry/Tracts/UniformTractogram.cs
+++ Assets/Scripts/Geometry/Tracts/UniformTractogram.cs
@@ -3,20 +3,10 @@ using System.Linq;
 using UnityEngine;
 
 namespace Geometry.Tracts {
-	public class UniformTractogram : Tractogram {
-		public IEnumerable<Tract> Tracts {get;}
-		public IEnumerable<Vector3> Points => Tracts.SelectMany(tract => tract.Points);
-		public Boundaries Boundaries => Boundaries.Join(Tracts.Select(segment => segment.Boundaries));
-		public float Resolution => Tracts.Select(segment => segment.Resolution).Min();
-		public float Slack => Tracts.Select(segment => segment.Slack).Max();
+	public class UniformTractogram : SimpleTractogram {
 		public int Samples => Tracts.Min(tract => tract.Points.Length);
 
-		public UniformTractogram(IEnumerable<Tract> lines) {
-			Tracts = lines;
-		}
-		public UniformTractogram Sample(int samples) {
-			return new UniformTractogram(Tracts.Select(tract => tract.Sample(samples)));
-		}
+		public UniformTractogram(IEnumerable<Tract> lines) : base(lines) {}
 		public IEnumerable<Vector3> Slice(int index) {
 			return Tracts.Select(tract => tract.Points[index]);
 		}

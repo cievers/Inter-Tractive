dbfbd14a6b3863890ee16ee43b8b44e8800f8d16
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-10-16 10:47:21 +0200

    Applied first test of automatic source loading

--- Assets/Scripts/Objects/SourceSequencer.cs
+++ Assets/Scripts/Objects/SourceSequencer.cs
@@ -1,33 +1,30 @@
-﻿using System.Collections.Generic;
-using Camera;
+﻿using Camera;
 using Objects.Sources;
 using UnityEngine;
 
 namespace Objects {
-	public class SourceSequencer : MonoBehaviour {
-		public Source template;
-		public ProjectionManager slices;
+	public class SourceSequencer : SourceManager {
+		public SourceAutomation template;
 		public new OrbitingCamera camera;
 
-		private readonly List<Source> sources = new();
+		private static readonly string[] SEQUENCE = {
+			"C:\\Users\\Cas\\Documents\\Computer Science\\Master\\Master Project\\Data\\Tract\\TOM\\T_POSTC_left.tck",
+			"C:\\Users\\Cas\\Documents\\Computer Science\\Master\\Master Project\\Data\\Tract\\TOM\\T_POSTC_right.tck"
+		};
 
 		private void Start() {
-			Add();
+			Add(SEQUENCE[0]);
 		}
-		public void Add() {
+		public void Add(string path) {
 			try {
 				var source = Instantiate(template, transform);
 
-				sources.ForEach(s => source.UpdateContext(s));
-				sources.Add(source);
-				sources.ForEach(s => s.UpdateContext(source));
+				source.Automate(path);
+				Collect(source);
+				Interact(source);
 
+				source.Loaded += loaded => Debug.Log("Automated source is loaded: " + loaded);
 				source.Focused += camera.Target;
-				source.Sliced += slices.Slice;
-				source.Closed += x => sources.Remove(x);
-				source.Closed += x => slices.Remove(x);
-				source.Closed += x => sources.ForEach(s => s.UpdateContext(x));
-				
 				if (sources.Count == 1) {
 					source.Focus();
 				}

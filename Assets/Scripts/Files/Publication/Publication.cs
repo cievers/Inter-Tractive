462f0026a697adee3c9edaa77ca4493c77d33077
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-10-18 11:20:34 +0200

    Ensured the directory always exists when writing a file

--- Assets/Scripts/Files/Publication/Publication.cs
+++ Assets/Scripts/Files/Publication/Publication.cs
@@ -1,10 +1,12 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 
 namespace Files.Publication {
 	public interface Publication {
 		public void Write(string path);
 		public static void Write(string path, byte[] data) {
+			Directory.CreateDirectory(Path.GetDirectoryName(path) ?? throw new InvalidOperationException("Cannot write to location outside any directories"));
 			using var stream = File.Open(path, FileMode.Create);
 			using var writer = new BinaryWriter(stream, Encoding.UTF8, false);
 			writer.Write(data);

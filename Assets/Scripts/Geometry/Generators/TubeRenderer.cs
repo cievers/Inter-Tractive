d0c468b76dbb2846672895b583928396214aa303
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-10-25 10:27:38 +0200

    Tube now uses the directional color encoding

--- Assets/Scripts/Geometry/Generators/TubeRenderer.cs
+++ Assets/Scripts/Geometry/Generators/TubeRenderer.cs
@@ -26,7 +26,8 @@ namespace Geometry.Generators {
 		private Model Route(Tract tract) {
 			var resultVertices = new Vector3[tract.Points.Length * vertices];
 			var resultNormals = new Vector3[tract.Points.Length * vertices];
-			var resultColors = Enumerable.Repeat(color, tract.Points.Length * vertices).ToArray();
+			var resultColors = new Color32[tract.Points.Length * vertices];
+			// var resultColors = Enumerable.Repeat(color, tract.Points.Length * vertices).ToArray();
 			var resultTriangles = new int[(tract.Points.Length - 1) * vertices * 6];
 
 			var segments = tract.Segments.ToArray();
@@ -39,11 +40,13 @@ namespace Geometry.Generators {
 			var startingDirections = Directions(startingAxisA, startingAxisB);
 			var currentVertices = startingDirections.Select(direction => tract.Points[0] + direction).ToArray();
 			var currentNormals = startingDirections.Select(direction => direction.normalized).ToArray();
+			var currentColor = Color(planes[0]);
 
-			var startingCap = Cap(currentVertices, tract.Points[0], -directions[0]);
+			var startingCap = Cap(currentVertices, tract.Points[0], -directions[0], currentColor);
 			
 			currentVertices.CopyTo(resultVertices, 0);
 			currentNormals.CopyTo(resultNormals, 0);
+			Enumerable.Repeat(currentColor, vertices).ToArray().CopyTo(resultColors, 0);
 
 			for (var i = 1; i < tract.Points.Length; i++) {
 				var origin = tract.Points[i];
@@ -68,16 +71,21 @@ namespace Geometry.Generators {
 				// 	triangles[j * 6 + 4] = (i - 1) * vertices + (j + 1) % vertices;
 				// 	triangles[j * 6 + 5] = i * vertices + (j + 1) % vertices;
 				// }
-				
+				currentVertices = intersections;
+				currentColor = Color(planes[i]);
+
 				intersections.CopyTo(resultVertices, i * vertices);
 				normals.CopyTo(resultNormals, i * vertices);
+				Enumerable.Repeat(currentColor, vertices).ToArray().CopyTo(resultColors, i * vertices);
 				triangles.CopyTo(resultTriangles, (i - 1) * vertices * 6);
-				currentVertices = intersections;
 			}
 
-			return Model.Join(startingCap, new Model(resultVertices, resultNormals, resultColors, resultTriangles), Cap(currentVertices, tract.Points[^1], directions[^1]));
+			return Model.Join(startingCap, new Model(resultVertices, resultNormals, resultColors, resultTriangles), Cap(currentVertices, tract.Points[^1], directions[^1], currentColor));
 		}
-		private Model Cap(Vector3[] points, Vector3 origin, Vector3 normal) {
+		private Model Cap(Vector3[] points, Vector3 origin, Vector3 normal, Color32 color) {
+			return Cap(points, origin, normal).Color(color);
+		}
+		private Hull Cap(Vector3[] points, Vector3 origin, Vector3 normal) {
 			if (Vector3.Dot(normal, Vector3.Cross(points[1] - points[0], points[2] - points[0])) <= 0) {
 				points = points.Reverse().ToArray();
 			}
@@ -114,7 +122,7 @@ namespace Geometry.Generators {
 				resultTriangles.AddRange(Polygon(length).Select(v => v + resultVertices.Count - length));
 			}
 			
-			return new Hull(resultVertices.ToArray(), resultNormals.ToArray(), resultTriangles.ToArray()).Color(color);
+			return new Hull(resultVertices.ToArray(), resultNormals.ToArray(), resultTriangles.ToArray());
 		}
 		private static IEnumerable<int> Cone(int vertices) {
 			var triangles = new int[vertices * 3];
@@ -172,6 +180,10 @@ namespace Geometry.Generators {
 		}
 		private Vector3[] Circle(Vector3 origin, Tuple<Vector3, Vector3> axis) {
 			return Circle(origin, axis.Item1, axis.Item2);
+		}
+
+		private Color32 Color(Vector3 normal) {
+			return new Color(Math.Abs(normal.x), Math.Abs(normal.z), Math.Abs(normal.y));
 		}
 	}
 }

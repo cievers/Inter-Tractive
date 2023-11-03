4c2dd0ec4ebbeb2be9c1d05cd953e4811f002fef
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-11-03 14:20:48 +0100

    Added utility constructors for small collections of objects of the same type

---
+++ Assets/Scripts/Objects/Collection/Pair.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Objects.Collection {
+	public class Pair<T> : Tuple<T, T> {
+		public Pair(T item1, T item2) : base(item1, item2) {}
+		public Pair(IList<T> items) : base(items[0], items[1]) {
+			if (items.Count > 2) {
+				throw new OverflowException("Provided too many items to a pair "+items.Count);
+			}
+		}
+	}
+}

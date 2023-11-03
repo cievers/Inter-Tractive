7562a211a291acc561ee51c050ce755bcebbb068
Cas Sievers <c.t.sievers@student.utwente.nl>
2023-11-03 14:22:28 +0100

    Created a utility for constructing circular walks of vectors

---
+++ Assets/Scripts/Geometry/Generators/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Geometry.Generators {
+	public static class Circle {
+		public static Walk Walk(Vector3 origin, Vector3 axisA, Vector3 axisB, int vertices) {
+			var result = new Vector3[vertices];
+			for(var i = 0; i < vertices; i++) {
+				var t = (float) i / vertices;
+				result[i] = origin + axisA * Mathf.Cos(2 * Mathf.PI * t) + axisB * Mathf.Sin(2 * Mathf.PI * t);
+			}
+			return new Walk(result);
+		}
+		public static Walk Walk(Vector3 origin, Tuple<Vector3, Vector3> axis, int vertices) {
+			return Walk(origin, axis.Item1, axis.Item2, vertices);
+		}
+	}
+}
